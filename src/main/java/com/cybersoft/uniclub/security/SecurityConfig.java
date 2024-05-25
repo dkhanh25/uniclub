5004ec760fb878876777c2ed34682aabc808df77
Quyet Pham <134134392+Decoded42@users.noreply.github.com>
2024-05-25 09:25:00 +0700

    Update SecurityConfig.java

--- src/main/java/com/cybersoft/uniclub/security/SecurityConfig.java
+++ src/main/java/com/cybersoft/uniclub/security/SecurityConfig.java
@@ -18,34 +18,13 @@ public class SecurityConfig {
 
     @Autowired
     private CustomJwtFilter customJwtFilter;
-
-    //Khởi tạo chuẩn mã hóa sử dụng và lưu trên IOC
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
-    /**
-     * Spring Security 6~
-     * Thay đổi thông tin cấu hình của Security : Phân quyền, chứng thực...
-     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//         http.csrf(c -> c.disable())
-//                .authorizeHttpRequests(a -> {
-//                            a.requestMatchers("/role").permitAll();
-//                            a.anyRequest().authenticated();
-//                        }
-//                );
-//
-//         return http.build();
-
-        /* Cấu hình Security như bên dưới
-         *  GET /admin -> Ai truy cập cũng được
-         *  POST /admin -> Chỉ user truy cập được
-         *  PUT /admin -> Chỉ admin vô được
-         *  DELETE /admin -> admin hoặc user vô được
-         */
 
         return http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
