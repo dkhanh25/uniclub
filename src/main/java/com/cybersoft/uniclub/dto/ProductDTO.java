6f1aceaaab92096978b9e04e7b48cf74d0048266
Quyet Pham <134134392+Decoded42@users.noreply.github.com>
2024-05-25 09:07:31 +0700

    Update ProductDTO.java

--- src/main/java/com/cybersoft/uniclub/dto/ProductDTO.java
+++ src/main/java/com/cybersoft/uniclub/dto/ProductDTO.java
@@ -3,11 +3,44 @@ package com.cybersoft.uniclub.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
-@Getter
-public class ProductDTO {
+@getter;
+@setter;
+public class ProductDTO implements Serializable {
     private int idSanPham;
     private String productName;
     private double price;
     private String image;
+
+    public int getIdSanPham() {
+        return idSanPham;
+    }
+
+    public void setIdSanPham(int idSanPham) {
+        this.idSanPham = idSanPham;
+    }
+
+    public string getProductName() {
+        return productName;
+    }
+
+    public void setProductName(string productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice (double price) {
+        this.price = price;
+    }
+
+    public string getImage() {
+        return image;
+    }
+
+    public void setImage(string image) {
+        this.image = image;
+    }
+    
 }
